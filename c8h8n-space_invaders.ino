edebc258cc31266bd072e8ac348b0a9cd7e9a7a4
maisanor-lgtm <maisanor@umich.edu>
2025-11-19 15:00:48 -0500

    Update c8h8n-space_invaders.ino
    3pm

--- c8h8n-space_invaders.ino
+++ c8h8n-space_invaders.ino
@@ -1,9 +1,7 @@
 #include <gamma.h>
 #include <RGBmatrixPanel.h>
 #include <Adafruit_GFX.h>
-
 #define FULLSCREEN_REFRESH
-
 // define the wiring of the LED screen
 const uint8_t CLK  = 8;
 const uint8_t LAT = A3;
@@ -11,22 +9,17 @@ const uint8_t OE = 9;
 const uint8_t A = A0;
 const uint8_t B = A1;
 const uint8_t C = A2;
-
 // define the wiring of the inputs
 const int POTENTIOMETER_PIN_NUMBER = 5;
 const int BUTTON_PIN_NUMBER = 10;
-
 // global constant for the number of Invaders in the game
 const int NUM_ENEMIES = 16;
-
 // a global variable that represents the LED screen
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
-
 // the following functions are for printing messages
 void print_level(int level);
 void print_lives(int lives);
 void game_over();
-
 class Color {
   public:
     int red;
@@ -46,7 +39,6 @@ class Color {
       return matrix.Color333(red, green, blue);
     }
 };
-
 const Color BLACK(0, 0, 0);
 const Color RED(4, 0, 0);
 const Color ORANGE(6, 1, 0);
@@ -57,36 +49,30 @@ const Color PURPLE(1, 0, 2);
 const Color WHITE(4, 4, 4);
 const Color LIME(2, 4, 0);
 const Color AQUA(0, 4, 4);
-
 class Invader {
   public:
     // Constructors
-    Invader() {
+   Invader() {
       x = 0;
       y = 0;
       strength = 0;
-      time = millis();
     }
     // sets values for private date members x and y
     Invader(int x_arg, int y_arg) {
       x = x_arg;
-      y = y_arg;
-      strength = 0;
-      time = millis();
+      y = y_arg; 
     }
     // sets values for private data members
     Invader(int x_arg, int y_arg, int strength_arg) {
       x = x_arg;
       y = y_arg;
-      strength = 0;
-      time = millis();
+      strength = strength_arg;
     }
     // sets values for private data members
     void initialize(int x_arg, int y_arg, int strength_arg) {
       x = x_arg;
       y = y_arg;
       strength = strength_arg;
-      time = millis();
     }
     
     // getters
@@ -103,29 +89,60 @@ class Invader {
     // Moves the Invader down the screen by one row
     // Modifies: y
     void move() {
-      time = millis();
-      if (time > last_time + 1750) {
-        erase();
-        y = y + 1;
-        draw();
-        last_time = time;
-      }
+       erase();
+       y++;
+      draw();
     }
     
     // draws the Invader if its strength is greater than 0
     // calls: draw_with_rgb
     void draw() {
+    Color temp;
+    Color eyes = BLUE;
+    if(0 >= get_strength() || 7 < get_strength()) {
+      erase();
+      return;
     }
-    
+    else if(1 == get_strength()) {
+      temp = RED;
+    }
+    else if(2 == get_strength()) {
+      temp = ORANGE;
+    }
+    else if(3 == get_strength()) {
+      temp = YELLOW;
+    }
+    else if(4 == get_strength()) {
+      temp = GREEN;
+    }
+    else if(5 == get_strength()) {
+      temp = BLUE;
+    }
+    else if(6 == get_strength()) {
+      temp = PURPLE;
+    }
+    else if(7 == get_strength()) {
+      temp = WHITE;
+    }
+    draw_with_rgb(temp, eyes);
+    return;
+    }
+
+
+
     // draws black where the Invader used to be
     // calls: draw_with_rgb
     void erase() {
+     draw_with_rgb(BLACK, BLACK);
     }    
     
     // Invader is hit by a Cannonball.
     // Modifies: strength
     // calls: draw, erase
     void hit() {
+    erase();
+    strength -= 1;
+    draw();
     }
 
   private:
@@ -135,6 +152,18 @@ class Invader {
     
     // draws the Invader
     void draw_with_rgb(Color body_color, Color eye_color) {
+      matrix.drawPixel((x + 1), y, body_color.to_333());
+      matrix.drawPixel((x + 2), y, body_color.to_333());
+      matrix.drawPixel(x, (x + 1), body_color.to_333());
+      matrix.drawPixel((x + 3), (y + 1), body_color.to_333());
+      matrix.drawPixel(x, (x + 2), body_color.to_333());
+      matrix.drawPixel((x + 1), (y + 2), body_color.to_333());
+      matrix.drawPixel((x + 2), (y + 2), body_color.to_333());
+      matrix.drawPixel((x + 3), (y + 2), body_color.to_333());
+      matrix.drawPixel(x, (x + 3), body_color.to_333());
+      matrix.drawPixel((x + 3), (y + 1), body_color.to_333());
+      matrix.drawPixel((x + 1), (y + 1), body_color.to_333());
+      matrix.drawPixel((x + 2), (y + 1), body_color.to_333());
     }
 };
 
@@ -144,13 +173,16 @@ class Cannonball {
       x = 0;
       y = 0;
       fired = false;
-      time = millis();
+      strength = 1; // I'm confused what this is doing here. This also have not been declared prior
+      time = 0; // dotto here.
     }
-    
+
     // resets private data members to initial values
     void reset() {
+      fired = false;
+      strength = 1;
     }
-    
+
     // getters
     int get_x() const {
       return x;
@@ -161,28 +193,42 @@ class Cannonball {
     bool has_been_fired() const {
       return fired;
     }
-    
+
     // sets private data members
     void fire(int x_arg, int y_arg) {
+      x = x_arg;
+      y = y_arg;
+      fired = true;
     }
     
     // moves the Cannonball and detects if it goes off the screen
     // Modifies: y, fired
     void move() {
       time = millis();
+      if (fired) {
+        erase();
+        y--;
+        if (y < 0) {
+          fired = false;
+        }
+        else {
+          draw();
+        }
+      }
     }
-    
+
     // resets private data members to initial values
     void hit() {
-      strength = strength - 1;
+      strength--;
       if (strength > 0) {
         draw();
       }
       else {
         erase();
-      
+
+      }
     }
-    
+
     // draws the Cannonball, if it is fired
     void draw() {
       //if invader has strength left, strength is corresponding color is assigned
@@ -194,16 +240,16 @@ class Cannonball {
         else if (strength == 2) {
           body_color = ORANGE;
         }
-        else if (strengh == 3) {
+        else if (strength == 3) {
           body_color = YELLOW;
         }
-        else if (strengh == 4) {
+        else if (strength == 4) {
           body_color = GREEN;
         }
-        else if (strengh == 5) {
+        else if (strength == 5) {
           body_color = BLUE;
         }
-        else if (strengh == 6) {
+        else if (strength == 6) {
           body_color = PURPLE;
         }
         else {
@@ -215,8 +261,9 @@ class Cannonball {
     else {
       draw_with_rgb(BLACK, BLACK);
     }
+
     }
-    
+
     // draws black where the Cannonball used to be
     void erase() {
       draw_with_rgb(BLACK, BLACK);
@@ -226,8 +273,9 @@ class Cannonball {
     int x;
     int y;
     bool fired;
+    int strength; // declaring strength
+    long time; // declaring class local time
 };
-
 class Player {
   public:
     Player() {
@@ -246,13 +294,11 @@ class Player {
     }
     int get_lives() const {
       return lives;
-
     }
     
     // setter
     void set_x(int x_arg) {
       x = x_arg;
-
     }
     
     // Modifies: lives
@@ -270,32 +316,20 @@ class Player {
     // draws black where the Player used to be
     // calls: draw_with_rgb
     void erase() {
-
       //matrix.fillScreen(0);
-
-
         matrix.drawPixel(x-2, y, 0);
         matrix.drawPixel(x+2, y, 0);
-
         matrix.drawPixel(x-1, y, 0);
         matrix.drawPixel(x+1, y, 0);
-
         matrix.drawPixel(x-1, y-1, 0);
         matrix.drawPixel(x+1, y-1, 0);
-
         matrix.drawPixel(x, y-1, 0);
         matrix.drawPixel(x, y, 0);
-
-
-
     }
-
-
   private:
     int x;
     int y;
     int lives;
-
     // sets private data members x and y to initial values
     void initialize(int x_arg, int y_arg) {
       x = x_arg;
@@ -310,19 +344,19 @@ class Player {
       matrix.drawPixel(x, y-1, color.to_333());
     }
 };
-
 class Game {
   public:
     Game() {
+      level = 0;
       level = 1;
       time = 0;
     }
-    
+
     // sets up a new game of Space Invaders
     // Modifies: global variable matrix
     void setupGame() {
+      Player player;
       delay(500);
-      btwo.draw();
       delay(100000000);
       delay(500);
       print_level(level);
@@ -339,7 +373,7 @@ class Game {
           enemies[i].draw();
           x_arg = x_arg + 4;
         }
-        
+
         y_arg = 5;
         x_arg = 1;
 
@@ -348,7 +382,7 @@ class Game {
           enemies[i].draw();
           x_arg = x_arg + 4;
         }
-        
+
       }
 
       else if (level == 2) {
@@ -421,9 +455,9 @@ class Game {
         }
       }
       player.draw();
-      
+
     }
-    
+
     // advances the game simulation one step and renders the graphics
     // see spec for details of game
     // Modifies: global variable matrix
@@ -431,7 +465,6 @@ class Game {
       player.set_x((potentiometer_value)/32);
       player.draw();
     }
-
   private:
     int level;
     unsigned long time;
@@ -458,12 +491,10 @@ class Game {
 
 // a global variable that represents the game Space Invaders
 Game game;
-
 // DEBUGGING / TESTING GLOBAL VARS
 long time = 0;
 int startX = 0;
 int startY = 0;
-
 // see https://www.arduino.cc/reference/en/language/structure/sketch/setup/
 void setup() {
   Serial.begin(9600);
@@ -473,14 +504,11 @@ void setup() {
   game.setupGame();
 
 }
-
 // see https://www.arduino.cc/reference/en/language/structure/sketch/loop/
 void loop() {
   int potentiometer_value = analogRead(POTENTIOMETER_PIN_NUMBER);
   bool button_pressed = (digitalRead(BUTTON_PIN_NUMBER) == HIGH);
-
   game.update(potentiometer_value, button_pressed);
-
   
 }
 
